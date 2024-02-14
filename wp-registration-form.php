aac2a09d7e5c7d8499f655baa3c04b1eaf87f2bc
Gazi Akter <gaziakterdev@gmail.com>
2024-02-14 07:55:09 +0600

    add custom field and save dynamicly

--- wp-registration-form.php
+++ wp-registration-form.php
@@ -20,6 +20,10 @@ class Custom_registration_form{
         add_action( 'register_form', array($this, 'custom_register_form') );
         add_filter('registration_errors', array($this, 'custom_register_error'), 10, 3 );
         add_action( 'user_register', array($this, 'input_user_data') );
+        add_action( 'edit_user_profile', array($this, 'custom_user_profile_fields') );
+        add_action( 'show_user_profile', array($this, 'custom_user_profile_fields') );
+        add_action( 'personal_options_update', array($this, 'update_user_profile_fields') );
+        add_action( 'edit_user_profile_update', array($this, 'update_user_profile_fields') );
     }
 
     function registration_form_load_textdomain() {
@@ -29,6 +33,7 @@ class Custom_registration_form{
     public function custom_register_form(){
         $first_name = $_POST['first_name'] ?? '';
         $last_name = $_POST['last_name'] ?? '';
+        $phone_number = $_POST['phone_number'] ?? '';
         ?>
         <p>
             <label>
@@ -42,6 +47,12 @@ class Custom_registration_form{
             </label>
             <input type="text" name="last_name" id="last_name" value="<?php echo esc_attr( $last_name ); ?>">    
         </p>
+        <p>
+            <label>
+                <?php _e('Phone Number', 'regi-form'); ?>
+            </label>
+            <input type="text" name="phone_number" id="phone_number" value="<?php echo esc_attr( $phone_number ); ?>">    
+        </p>
         <?php
     }
 
@@ -52,6 +63,10 @@ class Custom_registration_form{
 
         if(empty($_POST['last_name'])){
             $errors->add('last_name_blank', __('last Name can not be blank', 'regi-form'));
+        }
+
+        if(empty($_POST['phone_number'])){
+            $errors->add('phone_number_blank', __('Phone number can not be blank', 'regi-form'));
         }
 
         return $errors;
@@ -65,7 +80,41 @@ class Custom_registration_form{
         if(!empty($_POST['last_name'])){
             update_user_meta( $user_id, 'last_name', sanitize_text_field($_POST['last_name'] ) );
         }
+
+        if(!empty($_POST['phone_number'])){
+            update_user_meta( $user_id, 'phone_number', sanitize_text_field($_POST['phone_number'] ) );
+        }
+    }
+
+    public function custom_user_profile_fields( $user ) {
+        ?>
+            <table class="form-table">
+                <tr>
+                    <th>
+                        <label for="phone_number"><?php _e( 'Phone Number' ); ?></label>
+                    </th>
+                    <td>
+                        <input type="number" name="phone_number" id="phone_number" value="<?php echo esc_attr( get_user_meta( $user->ID, 'phone_number', true  ) ); ?>" class="regular-text ltr" />
+                        <br><span class="description"><?php _e( 'Your phone number.', 'regi-form' ); ?></span>
+			        </td>
+                </tr>
+            </table>
+        <?php
+    }
+
+
+    public function update_user_profile_fields($user_id){
+
+        if ( current_user_can( 'edit_user', $user_id ) ){
+            update_user_meta( $user_id, 'phone_number', sanitize_text_field( $_POST['phone_number'] ));
+       }
     }
 }
 
 new Custom_registration_form();
+
+
+
+
+add_action( 'personal_options_update', 'wporg_update_extra_profile_fields' );
+
